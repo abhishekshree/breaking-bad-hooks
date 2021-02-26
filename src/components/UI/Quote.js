e2d60be969db731d08a5d075a48005f2b1774252
Abhishek Shree <abhi1234456@gmail.com>
2021-02-26 21:11:24 +0530

    Name error fix

--- src/components/UI/Quote.js
+++ src/components/UI/Quote.js
@@ -3,13 +3,13 @@ import React, {useState, useEffect} from 'react'
 
 const Quote = () => {
     const [Quote, setQuote] = useState('')
-    const [Name, setName] = useState('')
+    // const [Name, setName] = useState('')
     useEffect(() => {
             const getQuote = async () => {
                 const quote = await axios(`https://www.breakingbadapi.com/api/quote/random`)
                 // console.log(quote.data[0])
                 setQuote(quote.data[0].quote)
-                setName(quote.data[0].author)
+                // setName(quote.data[0].author)
             }
             getQuote()
         },[])
