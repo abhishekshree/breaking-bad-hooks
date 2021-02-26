a31893d272edd26f5e68b1f451c84c50dfff5ef6
Abhishek Shree <abhi1234456@gmail.com>
2021-02-26 21:05:32 +0530

    Frontend in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './components/UI/Header'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/UI/Search'
+import Quote from './components/UI/Quote'
 
-function App() {
+const App = () => {
+  
+  const [items, setitems] = useState([])
+  const [isLoading, setisLoading] = useState(true)
+  const [query, setquery] = useState('')
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+      // console.log(result.data)
+      setitems(result.data)
+      setisLoading(false)
+    }
+
+    fetchItems()
+  }, [query])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header />
+        <Quote />
+        <Search getQuery={ (q)=> setquery(q) } />      
+        <CharacterGrid isLoading={isLoading} items={items} />
+      </div>
     </div>
   );
 }
